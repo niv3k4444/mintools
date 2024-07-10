d32d9a196e128850377c1c8cb4628bca1b652df9
niv3k4444 <123826643+niv3k4444@users.noreply.github.com>
2024-07-10 17:38:48 +0200

    Delete scriptLowercase.js

--- scriptLowercase.js
+++
@@ -1,8 +0,0 @@
-function lowerCase() {
-    var text = document.getElementById("textIn").value;
-    var result = text.toLowerCase();
-    document.getElementById('textOut').value = result;
-}
-
-
-
